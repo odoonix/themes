3bd5f274cda204accfafb66efae39a67e5de95e2
fardinmardani <fmardani0@gmail.com>
2026-04-05 15:49:44 +0330

    [UP]

--- elbaan_theme_backend/__manifest__.py
+++ elbaan_theme_backend/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Elbaan Theme",
-    "version": "18.0.1.0",
+    "version": "17.0.1.0",
     "category": "Themes/Services",
     "depends": ["base", "web", "main_theme_backend"],
     "author": "MoonSun",
