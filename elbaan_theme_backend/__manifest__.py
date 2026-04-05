d3cdf633f43d349a1ac1da7bb092db8567fabf0d
fardinmardani <fmardani0@gmail.com>
2026-04-05 14:35:21 +0330

    [UP]To 17

---
+++ elbaan_theme_backend/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Elbaan Theme",
+    "version": "18.0.1.0",
+    "category": "Themes/Services",
+    "depends": ["base", "web", "main_theme_backend"],
+    "author": "MoonSun",
+    "company": "MoonSun",
+    "website": "https://www.moonsun.au",
+    "description": "Elbaan Theme",
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "license": "LGPL-3",
+    "assets": {
+        "web._assets_primary_variables": [
+            "elbaan_theme_backend/static/src/scss/fonts.scss",
+            "elbaan_theme_backend/static/src/scss/theme.scss",
+            (
+                "before",
+                "web/static/src/scss/primary_variables.scss",
+                "elbaan_theme_backend/static/src/scss/variables.scss",
+            ),
+        ],
+    },
+}
